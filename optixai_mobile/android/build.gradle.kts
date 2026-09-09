b6a6a3172efa05c39d2a59588f3161230784a468
Aaryan <aaryansp2737@gmail.com>
2026-09-09 21:42:12 +0530

    Enforce JVM 17 for Kotlin & Java tasks to resolve compatibility issues

--- optixai_mobile/android/build.gradle.kts
+++ optixai_mobile/android/build.gradle.kts
@@ -22,3 +22,15 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+allprojects {
+    tasks.withType<org.gradle.api.tasks.compile.JavaCompile>().configureEach {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+}
